95fd707143e27c8c0982d130b7b5eb59be3680b9
Soheil <soheil.nazari@check24.de>
2024-06-11 22:27:44 +0200

    init

---
+++ src/addition.ts
@@ -0,0 +1,9 @@
+/**
+ * @deprecated Use the + operand instead
+ * @param a first addend
+ * @param b second addent
+ * @returns sum
+ */
+export function addition(a: number, b: number): number {
+  return a + b;
+}
